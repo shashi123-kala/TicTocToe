315c8abc7acb8a10d8c2c0ea55382d6951dd15e6
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-07-20 09:09:42 +0530

    Add: Initialize Dash board

--- src/main/java/com/ttt/tdd/TicTacToe.java
+++ src/main/java/com/ttt/tdd/TicTacToe.java
@@ -4,6 +4,8 @@ import com.ttt.exception.TicTacToeException;
 
 public class TicTacToe {
 
+	private Character[][] board = { { '\0', '\0', '\0' }, { '\0', '\0', '\0' }, { '\0', '\0', '\0' } };
+
 	public void play(int column, int row) {
 		if (column < 1 || column > 3) {
 			throw new TicTacToeException("X value is outside the board!");
