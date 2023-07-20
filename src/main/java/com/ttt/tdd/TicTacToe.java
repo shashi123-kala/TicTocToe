fe89bd0b1561796c092d59d898a34e537cd094c3
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-07-20 10:56:11 +0530

    Refractor: minimize the code winner method

--- src/main/java/com/ttt/tdd/TicTacToe.java
+++ src/main/java/com/ttt/tdd/TicTacToe.java
@@ -22,12 +22,10 @@ public class TicTacToe {
 	private boolean isWinner() {
 		int playerTotal = lastPlayer * SIZE;
 		for (int i = 0; i < SIZE; i++)
-			if (board[0][i] + board[1][i] + board[2][i] == playerTotal) {
-                return true;
-            }
-            else if (board[i][0] + board[i][1] + board[i][2] == playerTotal) {
-                return true;
-            }
+			if (board[0][i] + board[1][i] + board[2][i] == playerTotal
+					|| board[i][0] + board[i][1] + board[i][2] == playerTotal) {
+				return true;
+			}
 
 		return false;
 	}
